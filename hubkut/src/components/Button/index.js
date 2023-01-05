e527990fc4e967cb86d9fe92f6c0e62d5bf2d462
Ulisses Marciano Gonçalves <104742158+ulissesmarciano@users.noreply.github.com>
2023-01-05 13:05:03 -0300

    To Do Section dinâmica.

--- hubkut/src/components/Button/index.js
+++ hubkut/src/components/Button/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { ButtonContainer } from './styles'
 
-const Button = ({variant="primary", href, title}) => {
+const Button = ({variant="primary", href, title }) => {
   return (
-    <ButtonContainer variant={variant} href={href}>
+    <ButtonContainer variant={variant} href={href} target="_blank">
       <p>{title}</p>
     </ButtonContainer>
   )
--- hubkut/src/pages/homescreen/section/toDoSection/index.js
+++ hubkut/src/pages/homescreen/section/toDoSection/index.js
@@ -9,10 +9,10 @@ const ToDoSection = () => {
         <h3>O que você deseja fazer?</h3>
       </div>
       <ButtonContainer>
-        <Button title='Criar repositório'/>
+        <Button title='Criar repositório' href="https://github.com/new"/>
         <Button variant={!'primary'} title='Clonar Repositório'/>
         <Button variant={!'primary'}  title='Suas Estrelas'/>
-        <Button variant={!'primary'} title='Suas Estrelas'/>
+        <Button variant={!'primary'} title='Novo Gist'/>
       </ButtonContainer>
     </Container>
   )
